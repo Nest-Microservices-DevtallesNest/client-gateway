97dbcf43d714aab9175a68f0326be446eeb24a64
Leandro Matus <leandromatus85@gmail.com>
2025-01-02 14:17:41 -0600

    feat: integrate microservices support in products module and update environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { envs } from 'config';
+import { envs } from 'src/config';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
